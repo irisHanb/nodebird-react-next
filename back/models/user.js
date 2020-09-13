c813e1a24946cd8b3f2dee5d2867f5ecd70b1024
irisHanb <fortrulymylife@gmail.com>
2020-09-13 09:48:10 +0900

    수정: sequlize model 문법 class 로

--- back/models/user.js
+++ back/models/user.js
@@ -1,27 +1,34 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      email: {
-        type: DataTypes.STRING(30),
-        allowNull: false, // 필수
-        unique: true,
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Comment extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        email: {
+          type: DataTypes.STRING(30),
+          allowNull: false, // 필수
+          unique: true,
+        },
+        nickname: {
+          type: DataTypes.STRING(30),
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
       },
-      nickname: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-    },
-    {
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-    }
-  );
-  User.associate = (db) => {
+      {
+        modelName: 'User',
+        tableName: 'users',
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        sequelize,
+      }
+    );
+  }
+  static associate(db) {
     db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
     db.User.belongsToMany(db.User, {
       through: 'Follow',
@@ -35,6 +42,5 @@ module.exports = (sequelize, DataTypes) => {
     });
     db.User.hasMany(db.Post);
     db.User.hasMany(db.Comment);
-  };
-  return User;
+  }
 };
