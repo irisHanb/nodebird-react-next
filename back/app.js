c52c9cbb0f1a2c594d98a7fcc9aab22dd9240ced
irisHanb <fortrulymylife@gmail.com>
2020-09-30 20:45:18 +0900

    add: root get send

--- back/app.js
+++ back/app.js
@@ -49,6 +49,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res) => {
+  res.send('hello express');
+})
+
 app.use('/user', userRouter);
 app.use('/post', postRouter);
 app.use('/posts', postsRouter);
