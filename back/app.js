ebf3711c4f7f898431dab82da123336c4e400637
irisHanb <fortrulymylife@gmail.com>
2020-10-02 12:08:53 +0900

    add: back cors origin

--- back/app.js
+++ back/app.js
@@ -32,16 +32,23 @@ if( process.env.NODE_ENV === 'production'){
   app.use(morgan('combined'));
   app.use(hpp());
   app.use(helmet());
+  app.use(
+    cors({    
+      origin: '*',
+      credentials: true,
+    })
+  );
 }else{
   app.use(morgan('dev'));
+  app.use(
+    cors({    
+      origin: true,
+      credentials: true,
+    })
+  );
 }
 
-app.use(
-  cors({    
-    origin: ['http://localhost:3060', 'nodebird.com' , 'http://13.209.89.42'],
-    credentials: true,
-  })
-);
+
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
